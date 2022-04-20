7f4391395a67e858cad386ede1933379bf44ac0e
Gajendra K <gajendra.mahadeva@neosoftmail.com>
2022-04-20 18:07:18 +0530

    update db

--- src/helper/db.connect.js
+++ src/helper/db.connect.js
@@ -19,7 +19,7 @@ mongoose.connection.on('error', (err) => {
   console.log(err.message)
 })
 
-mongoose.connection.on('disconnected', () => {
+mongoose.connection.on('disconnected', (err) => {
   console.log('Mongoose connection is disconnected.')
 })
 
